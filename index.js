4cbb4fdced2143988a315ab8a5fe7beb086c06a9
Kevin Colten <kevin@austincodingacademy.com>
2020-03-08 19:45:56 -0500

    port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const googleMapsClient = require('@google/maps').createClient({
   key: process.env.GOOGLE_API_KEY,
   Promise: Promise,
